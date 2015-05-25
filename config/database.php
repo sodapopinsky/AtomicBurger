d6136fbea92de799bb8d5c315d2a75ca9dd8f011
sodapopinsky <Nick@Nicholass-iMac.local>
2015-05-25 18:22:39 -0500

    f

--- config/database.php
+++ config/database.php
@@ -71,10 +71,10 @@ return [
 
 		'pgsql' => [
 			'driver'   => 'pgsql',
-			'host'     => env($host, 'localhost'),
-			'database' => env($database, 'atomicburger'),
-			'username' => env($username, 'Nick'),
-			'password' => env($password, ''),
+			'host'     => env($host),  //localhost
+			'database' => env($database),  //atomicburger
+			'username' => env($username),  //Nick
+			'password' => env($password), //''
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
